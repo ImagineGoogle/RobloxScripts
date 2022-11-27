d9f6926a9cc3d7c5c427e8ec4a852c64bccea698
ImagineGoogle <113044328+ImagineGoogle@users.noreply.github.com>
2022-11-26 21:08:02 -0500

    Update TelanthricBedWars.lua

--- TelanthricBedWars.lua
+++ TelanthricBedWars.lua
@@ -1,6 +1,22 @@
 repeat task.wait() until game:IsLoaded()
 
-local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
-queueteleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineGoogle/CapeForRobloxBedwars/main/MainScript.lua", true))()')
+local queueTeleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
+queueTeleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineGoogle/RobloxScripts/main/TelanthricBedWars.lua", true))()')
 
 local GuiLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/ImagineGoogle/CapeForRobloxBedwars/main/GuiLibrary.lua"), true)()
+
+GuiLibrary:CreateMainGui()
+
+GuiLibrary:CreateWindow("Combat")
+GuiLibrary:CreateWindow("Blatant")
+GuiLibrary:CreateWindow("Render")
+GuiLibrary:CreateWindow("Utility")
+GuiLibrary:CreateWindow("World")
+
+GuiLibrary:CreateModule("Blatant", "KillAura", function(callback)
+    if callback then
+        print("Enabled")
+    else
+        print("Disabled")
+    end
+end)
